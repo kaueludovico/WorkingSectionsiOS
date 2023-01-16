66410ea32eee5c8b10557e4c7f68b1c6b591133a
kaueludovico <kaueludovico@outlook.com>
2023-01-15 23:37:19 -0300

    Initial Commit

---
+++ AppleRocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleRocket
+//
+//  Created by Kaue Ludovico on 15/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
